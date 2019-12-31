b244119178ea7ba1b78b625631b2bdfed6a472ea
Dwayne Munro <dwaynemunro1845@gmail.com>
2019-12-31 12:18:05 -0500

    Javascript objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-let cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png",
+    },
+    {
+       rank: "queen",
+       suit: "diamonds",
+       cardImage: "images/queen-of-diamonds.png",
+    },
+ 
+    {
+     rank: "king",
+     suit: "hearts",
+     cardImage: "images/king-of-hearts.png",
+    },
+     {
+     rank: "king",
+     suit: "diamonds",
+     cardImage: "images/king-of-diamonds.png",
+    },
+ ];
 
 let cardOne = cards[0];
 let cardTwo = cards[2];
@@ -10,16 +32,16 @@ cardsInPlay.push(cardTwo);
 
 function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
+        alert("You found a match!");
       } else {
-        console.log("Sorry, try again.");
+        alert("Sorry, try again.");
     }
 }
 
 function flipCard(cardId) {
     checkForMatch();
-    cardsInPlay.push(cards[cardId]);
-    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cardsInPlay.push(cards[cardId].rank));
+    console.log("User flipped " + cards[cardId].rank + " " + cards[cardId].suit + " " + cards[cardId].cardImage);
 }
 
 flipCard(0);
